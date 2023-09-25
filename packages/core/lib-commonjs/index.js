2f11dd534c0605bcc06cf40fd760b324d3dde64d
pfmartins4 <pfmartins4@gmail.com>
2023-09-25 12:17:45 +0100

    Delete packages/core/lib-commonjs directory

--- packages/core/lib-commonjs/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.helloWorld = void 0;
-var start_1 = require("./start/start");
-Object.defineProperty(exports, "helloWorld", { enumerable: true, get: function () { return start_1.helloWorld; } });
-//# sourceMappingURL=index.js.map
